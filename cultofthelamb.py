259c4957f0b169295d1c46967be2bb84b2d6a7e7
Vzrod <71232916+Vzrod@users.noreply.github.com>
2022-11-23 21:18:17 +0100

    V1.14

--- cultofthelamb.py
+++ cultofthelamb.py
@@ -11,28 +11,32 @@ clear = lambda: os.system('cls')
 class Game:
     
     def __init__(self):
-        #Initialisation plateaux + numéros de joueur
-        self.plateaux=[[[0,1,0],[0,2,0],[0,0,0]],[[0,1,0],[0,3,0],[0,0,0]]]
-        self.J1=int(0)
-        self.J2=int(1)
+        """
+        Initialisation plateaux + numéros de player , plateaux[player][ligne (bas --> haut)][case (gauche --> droite)] 
+        """
+        self.plateaux=[[[1,5,2],[0,5,2],[0,5,0]],[[0,1,0],[0,3,0],[0,0,0]]]
+        self.P1=int(0)
+        self.P2=int(1)
+        self.scores = [0, 0]
         
     def tirage_de(self):
         #Nombre random int entre 1 et 6
         self.devalue=random.randint(1, 6)
         return self.devalue
         
-    def jouer(self, joueur):
-        #Input joueur choix colonne
+    def play(self, player):
+        #Input player choix colonne
+        self.playedColums=0
         while True:
-            y = int(input("Choix de la colonne entre 1 et 3 : "))
-            y -= 1 #Pour correspondre aux tableaux
-            if y <= 2 and y >= 0:
+            self.playedColums = int(input("Choix de la colonne entre 1 et 3 : "))
+            self.playedColums -= 1 #Pour correspondre aux tableaux
+            if self.playedColums <= 2 and self.playedColums >= 0:
                 break
         #Calcul position vertical (gravité)
         self.playable = True
         self.ligne = 0
         while True:
-            self.val_ligne = self.plateaux[joueur][self.ligne][y]
+            self.val_ligne = self.plateaux[player][self.ligne][self.playedColums]
             if self.val_ligne != 0 and self.ligne < 2:
                 self.ligne += 1
             elif self.val_ligne == 0:
@@ -40,24 +44,37 @@ class Game:
             else:
                 self.playable = False
                 break
-        #Mise à jour plateaux (nJoueur, ligne, colonne)
+        #Mise à jour plateaux (nplayer, ligne, colonne)
         if self.playable == True:
-            self.plateaux[joueur][self.ligne][y]=self.tirage_de()
+            self.plateaux[player][self.ligne][self.playedColums]=self.tirage_de()
         else:
             print("La colonne est deja pleine")
-            self.jouer(joueur)
-        y=0
+            self.play(player)
+     
+
+    def enemyVerif(self, player):
+        enemy = 1 - player
+        for i in range(0,3):
+            if self.plateaux[enemy][self.playedColums][i] == self.devalue: 
+                self.plateaux[enemy][self.playedColums][i] = 0
+            
             
         
-    def calcul_point(self, joueur):
-         
+
         
-        
-        
-        joueur.point=int()
+    def point(self, player):
+         colonne = [0, 0, 0]
+         score = [0, 0, 0]
+         for i in range(0,3):
+             for y in range(0,3):
+                 colonne[y] = self.plateaux[player][y][i]
+             for x in range(1,7):
+                 score[i] += (colonne.count(x)**2)*x
+         self.scores[player] = sum(score)
             
-    def afficher_plateau(self):
-        #Affiche plateau J1
+            
+    def printPlate(self):
+        #Affiche plateau P1
         for i in reversed(range(0,3)):
             for j in range(0,3):
                 print("|", end="")
@@ -67,7 +84,7 @@ class Game:
                     print(self.plateaux[0][i][j], end="")
             print("|")
         print("-------")
-        #Affiche plateau J2
+        #Affiche plateau P2
         for i in range(0,3):
             for j in range(0,3):
                print("|", end="")
